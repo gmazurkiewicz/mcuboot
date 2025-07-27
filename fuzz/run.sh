a1f47df0626ad0e4fb51eb74452ebbf8cd26e279
Grzegorz Mazurkiewicz <mazurkiewicz.g@gmail.com>
2025-07-27 22:39:02 +0200

    Add compile command and vscode properties for fuzz

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -3,6 +3,6 @@
 
 set -e
 
-cmake -G Ninja -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19 -DFUZZTEST_FUZZING_MODE=on -S . -B build
+cmake -G Ninja -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19 -DFUZZTEST_FUZZING_MODE=on -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
 cmake --build build
 build/fuzz_test  --fuzz=MyTestSuite.IntegerAdditionCommutes
