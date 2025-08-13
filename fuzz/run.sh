435de41b3ef8ce8be3ce226e54637bcd299dc531
Grzegorz Mazurkiewicz <mazurkiewicz.g@gmail.com>
2025-08-13 17:56:56 +0200

    Add dockerfile and modify files to pass the compilation in docker

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -5,4 +5,4 @@ set -e
 
 cmake -G Ninja -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19 -DFUZZTEST_FUZZING_MODE=on -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
 cmake --build build
-build/fuzz_test  --fuzz=MyTestSuite.IntegerAdditionCommutes
+build/fuzz_test --fuzz=MyTestSuite.IntegerAdditionCommutes
