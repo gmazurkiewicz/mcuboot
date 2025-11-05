6d5f63d503cc5c5a5205839126662cb9b258b6b0
Grzegorz Mazurkiewicz <mazurkiewicz.g@gmail.com>
2025-11-05 22:02:45 +0100

    Turn off fuzzing and change build type to debug

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -3,6 +3,7 @@
 
 set -e
 
-cmake -G Ninja -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19 -DFUZZTEST_FUZZING_MODE=on -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19 -DFUZZTEST_FUZZING_MODE=off -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
 cmake --build build
-build/fuzz_test --fuzz=McuBootSuite.InvokeBootGo
+# build/fuzz_test --fuzz=McuBootSuite.InvokeBootGo
+build/fuzz_test
